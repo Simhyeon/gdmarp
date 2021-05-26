24e3c8327295392e9da98aab261c4ae85a8b46fb
simoncreek <simoncreek@tutanota.com>
2021-05-26 11:03:34 +0900

    Added template for advanced macro related bash scripts

---
+++ m4_ext/advanced_image.bash
@@ -0,0 +1,11 @@
+# Get value as json
+# Input should be 
+# {
+# 	src: [],
+# 	attr: []
+# }
+
+# Save src array
+src_array
+# Save attribute array
+attr_array
---
+++ m4_ext/advanced_text.bash
@@ -0,0 +1,11 @@
+# Get value as json
+# Input should be 
+# {
+# 	src: "",
+# 	attr: []
+# }
+
+# Save src array
+src_text
+# Save attribute array
+attr_array
