10c65a29c9758e7dd07c7d2fcafc154c559805e1
simoncreek <simoncreek@tutanota.com>
2021-06-15 19:45:40 +0900

    Moved env into designated module's env

--- modules/repr/compress.bash
+++ modules/repr/compress.bash
@@ -1,30 +1,31 @@
+#!/bin/sh
+
 FILE=$1
 BASE=${FILE%%.*}
 EXT=${FILE##*.}
-DIR=$(dirname "${FILE}")
 
 # Basic workflow
 # Get file as input
 # Compress the file and send compressed file's name
 # - Compression program : Optipng for png, jpegoptim for jpeg
 
-if [[ "$EXT" = "jpg" ]] || [[ "$EXT" = "jpeg" ]]; then
+if [ "$EXT" = "jpg" ] || [ "$EXT" = "jpeg" ]; then
 	new_name="$BASE"_comp.jpg
 
-	if ! [[ -f "$new_name" ]]; then
-		cp $FILE $new_name
-		jpegoptim $new_name > /dev/null
+	if ! [ -f "$new_name" ]; then
+		cp "$FILE" "$new_name"
+		jpegoptim "$new_name" > /dev/null
 	fi
-	printf "$new_name"
-elif [[ "$EXT" = "png" ]]; then
+	printf '..%s..' "$new_name"
+elif [ "$EXT" = "png" ]; then
 	new_name="$BASE"_comp.png
 
-	if ! [[ -f "$new_name" ]]; then
-		cp $FILE $new_name
-		optipng $new_name > /dev/null
+	if ! [ -f "$new_name" ]; then
+		cp "$FILE" "$new_name"
+		optipng "$new_name" > /dev/null
 	fi
-	printf "$new_name"
+	printf '..%s..' "$new_name"
 else # Unsupported extesnio file is not processed
-	echo $FILE
+	echo "$FILE"
 	exit 1;
 fi
---
+++ modules/repr/env.m4
@@ -0,0 +1,7 @@
+divert(`-1')
+# Change value to configure
+define(`v_basis_height', `500')dnl
+define(`v_font_default', `24')dnl
+define(`v_thead_default', `18')dnl
+define(`v_bin_sqlite', `sqlite3')dnl
+divert`'dnl
