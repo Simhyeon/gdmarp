06dca0f85f7e28f1f372b480934bcaa90dfb692b
simoncreek <simoncreek@tutanota.com>
2021-07-06 22:28:19 +0900

    Changed file extension + made a fallback

--- m4/scripts/parse_inc.sh
+++ m4/scripts/parse_inc.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
 PATH=$1
-CAND=inc/"$PATH".gdt
+CAND=inc/"$PATH".gddt
+CAND2=inc/"$PATH"
 
 # If candiate file doesn't exist
 if [ -f "$CAND" ]; then
 	printf '..%s..' "$CAND"
+elif [ -f "$CAND2" ]; then
+	printf '..%s..' "$CAND2"
 else
 	printf '..%s..' "$PATH"
 fi
