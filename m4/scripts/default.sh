772ed73f2716cb0deadac4371e923ab3dc79ab5a
simoncreek <simoncreek@tutanota.com>
2021-07-28 17:29:07 +0900

    Added comments

--- m4/scripts/default.sh
+++ m4/scripts/default.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# This scripts gets two arguments
+# First is a value to evaluate
+# Second is the default value to print
+# If first value is 0, then print default value
+# if not, print given input value as it is
+
 input=$(echo "$1" | xargs --)
 default_value=$2
 
--- m4/scripts/rm_comments.sed
+++ m4/scripts/rm_comments.sed
@@ -1,2 +1,4 @@
 # Remove all lines starting with double forward slashes but don't remove urls
+# Exclamation negates given regex. In his case negate a case where double slash
+# comes after colon(:) e.g. http:// or ftp:// are all ignored and preserverd
 /[:]\/\//! s/\/\/.*//g
